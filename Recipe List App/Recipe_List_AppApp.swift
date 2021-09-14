1e8cd8ed9c189d245f8bbcad526a27f6531a6496
Muhammadjon <20mamarasulov@gmail.com>
2021-09-14 09:17:39 +0900

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Muhammadjon Mamarasulov on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
